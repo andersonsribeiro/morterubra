d216714b4ea8083c74ee5b99e330821c6a0aa532
Anderson Ribeiro <ander.silva.ribeiro@gmail.com>
2024-05-27 01:30:27 -0300

    email sac

--- morterubra-app/src/components/Footer.tsx
+++ morterubra-app/src/components/Footer.tsx
@@ -3,6 +3,15 @@ export default function Footer() {
     <footer className="text-center py-16 bg-red-800">
       <h3 className=" text-2xl font-sans font-bold text-black">MORTE RUBRA</h3>
       <p className=" my-3">
+        E-mail:{" "}
+        <a
+          className=" underline hover:text-white"
+          href="mailto:sac@morterubra.com.br"
+        >
+          sac@morterubra.com.br
+        </a>
+      </p>
+      <p className=" my-3">
         <a
           className=" underline hover:text-white"
           href="https://instagram.com/morterubrashop"
