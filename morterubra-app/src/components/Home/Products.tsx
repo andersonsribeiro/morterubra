660385ed097d5902d6ea54e5c4855cbd20978f14
Anderson Ribeiro <ander.silva.ribeiro@gmail.com>
2024-08-19 06:35:45 -0300

    drop melodeath

--- morterubra-app/src/components/Home/Products.tsx
+++ morterubra-app/src/components/Home/Products.tsx
@@ -2,17 +2,14 @@ import hanoi_stray from "../../assets/img/LOJA_CAMISETA-THUMB-STRAY-A.png";
 import hanoi_confidence from "../../assets/img/LOJA_CAMISETA-THUMB-CONFIDENCE-A.png";
 import hanoi_under from "../../assets/img/LOJA_CAMISETA-THUMB-UNDER-A.png";
 import hanoi_raven from "../../assets/img/LOJA_CAMISETA-THUMB-THE-RAVEN-A.png";
-import witchfinder from "../../assets/img/LOJA_CAMISETA-THUMB-WITCHFINDER.jpg";
-import trouble from "../../assets/img/LOJA_CAMISETA-THUMB-TROUBLE-PSALM.jpg";
-import cathedral from "../../assets/img/LOJA_CAMISETA-THUMB-CATHEDRAL.jpg";
-import woods from "../../assets/img/LOJA_CAMISETA-THUMB-WOODS.jpg";
-import rhapsody from "../../assets/img/LOJA_CAMISETA-THUMB-RHAPSODY.png";
-import darkmoor from "../../assets/img/LOJA_CAMISETA-THUMB-DARKMOOR.png";
-import malicebanner from "../../assets/img/MALICE-BANNER-DESK.jpg";
 import gammaray from "../../assets/img/LOJA_CAMISETA-THUMB-GAMME-RAY.jpg";
 import domine from "../../assets/img/LOJA_CAMISETA-THUMB-DOMINE.jpg";
 import losthorizon from "../../assets/img/LOJA_CAMISETA-THUMB-LOST-HORIZON.jpg";
 import stratovarius from "../../assets/img/LOJA_CAMISETA-THUMB-STRATOVARIUS.jpg";
+import inflames from "../../assets/img/LOJA_CAMISETA-THUMB-INFLAMES-A.jpg";
+import gatesofishtar from "../../assets/img/LOJA_CAMISETA-THUMB-GATES-A.jpg";
+import unanimated from "../../assets/img/LOJA_CAMISETA-THUMB-UNANIMATED-A.jpg";
+import darktranquillity from "../../assets/img/LOJA_CAMISETA-THUMB-DARK-TRANQUILLITY-A.jpg";
 
 export default function Products() {
   return (
@@ -22,6 +19,111 @@ export default function Products() {
           id="section-1"
           className=" max-w-5xl mx-auto flex flex-wrap justify-center gap-4"
         >
+          <li className="flex flex-col">
+            <figure>
+              <img
+                src={darktranquillity}
+                alt="Camiseta: Dark Tranquillity - The Gallery"
+              />
+            </figure>
+            <h3 className=" font-sans font-bold text-3xl text-red-600">
+              Dark Tranquillity
+            </h3>
+            <h4 className=" font-sans font-bold">The Gallery</h4>
+
+            <p>
+              <del className="block text-xl">de R$ 100,00</del>
+              <b className="block text-2xl">por R$ 95,00</b>
+              <small className=" block">em até 3x sem juros</small>
+            </p>
+            <p>
+              <a
+                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
+              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
+                href="https://loja.morterubra.com.br/dark-tranquillity-the-gallery-6gugb"
+              >
+                IR PARA A LOJA
+              </a>
+            </p>
+          </li>
+          <li className="flex flex-col">
+            <figure>
+              <img
+                src={unanimated}
+                alt="Camiseta: Unanimated - Ancient God of Evil"
+              />
+            </figure>
+            <h3 className=" font-sans font-bold text-3xl text-red-600">
+              Unanimated
+            </h3>
+            <h4 className=" font-sans font-bold">Ancient God of Evil</h4>
+            <p>
+              <del className="block text-xl">de R$ 100,00</del>
+              <b className="block text-2xl">por R$ 95,00</b>
+              <small className=" block">em até 3x sem juros</small>
+            </p>
+            <p>
+              <a
+                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
+              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
+                href="https://loja.morterubra.com.br/unanimated-ancient-god-of-evil-iykav"
+              >
+                IR PARA A LOJA
+              </a>
+            </p>
+          </li>
+          <li className="flex flex-col">
+            <figure>
+              <img
+                src={gatesofishtar}
+                alt="Camiseta: Gates of Ishtar - A Bloodred Path"
+              />
+            </figure>
+            <h3 className=" font-sans font-bold text-3xl text-red-600">
+              Gates of Ishtar
+            </h3>
+            <h4 className=" font-sans font-bold">A Bloodred Path</h4>
+
+            <p>
+              <del className="block text-xl">de R$ 100,00</del>
+              <b className="block text-2xl">por R$ 95,00</b>
+              <small className=" block">em até 3x sem juros</small>
+            </p>
+            <p>
+              <a
+                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
+              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
+                href="https://loja.morterubra.com.br/gates-of-ishtar-a-bloodred-path-ei0w7"
+              >
+                IR PARA A LOJA
+              </a>
+            </p>
+          </li>
+          <li className="flex flex-col">
+            <figure>
+              <img src={inflames} alt="Camiseta: In Flames - The Jester Race" />
+            </figure>
+            <h3 className=" font-sans font-bold text-3xl text-red-600">
+              In Flames
+            </h3>
+            <h4 className=" font-sans font-bold">The Jester Race</h4>
+
+            <p>
+              <del className="block text-xl">de R$ 100,00</del>
+              <b className="block text-2xl">por R$ 95,00</b>
+              <small className=" block">em até 3x sem juros</small>
+            </p>
+            <p>
+              <a
+                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
+              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
+                href="https://loja.morterubra.com.br/in-flames-the-jester-race-hssn2"
+              >
+                IR PARA A LOJA
+              </a>
+            </p>
+          </li>
+
           <li className="flex flex-col">
             <figure>
               <img
@@ -128,153 +230,6 @@ export default function Products() {
               </a>
             </p>
           </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={darkmoor} alt="Camiseta: Dark Moor - The Gates of" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Dark Moor
-            </h3>
-            <h4 className=" font-sans font-bold">The Gates of Oblivion</h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/dark-moor-the-gates-of-oblivion-a7n6w"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={rhapsody} alt="Camiseta: Rhapsody - Legendary Tales" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Rhapsody
-            </h3>
-            <h4 className=" font-sans font-bold">Legendary Tales</h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/rhapsody-legendary-tales-ba57p"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={woods} alt="Camiseta - Woods of Ypres" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Woods of Ypres
-            </h3>
-            <h4 className=" font-sans font-bold">
-              Woods 5: Grey Skies & Electric Light
-            </h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/woods-of-ypres-woods-5-grey-skies-e-electric-light-j9dam"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={cathedral} alt="Camiseta - Cathedral" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Cathedral
-            </h3>
-            <h4 className=" font-sans font-bold">Forest of Equilibrium</h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/cathedral-forest-of-equelibrium-f5z3u"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={trouble} alt="Camiseta - Trouble" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Trouble
-            </h3>
-            <h4 className=" font-sans font-bold">Psalm 9</h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/trouble-psalm-9-jyynk"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-          <li className="flex flex-col">
-            <figure>
-              <img src={witchfinder} alt="Camiseta - Witchfinder General" />
-            </figure>
-            <h3 className=" font-sans font-bold text-3xl text-red-600">
-              Witchfinder General
-            </h3>
-            <h4 className=" font-sans font-bold">Death Penalty</h4>
-
-            <p>
-              <del className="block text-xl">de R$ 100,00</del>
-              <b className="block text-2xl">por R$ 95,00</b>
-              <small className=" block">em até 3x sem juros</small>
-            </p>
-            <p>
-              <a
-                className="block my-4 border-2 text-red-600 rounded-full py-3 px-5 border-red-600
-              hover:bg-red-600 hover:text-amber-100 hover:font-bold transition"
-                href="https://loja.morterubra.com.br/witchfinder-general-death-penalty-j38iy"
-              >
-                IR PARA A LOJA
-              </a>
-            </p>
-          </li>
-
           <li className="flex flex-col">
             <figure>
               <img
